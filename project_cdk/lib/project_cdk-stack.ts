e183b2ee518584a9444f9fcd98cce11dc1d37d96
tyler303047 <tyler303047@yahoo.com>
2023-03-19 18:57:10 -0500

    Added auth to github

--- project_cdk/lib/project_cdk-stack.ts
+++ project_cdk/lib/project_cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import {GitHubSourceAction} from "aws-cdk-lib/aws-codepipeline-actions";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ProjectCdkStack extends cdk.Stack {
@@ -10,7 +11,9 @@ export class ProjectCdkStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('tyler303047/AWS_Discord_Bot_Service', 'main'),
+        input: CodePipelineSource.gitHub('tyler303047/AWS_Discord_Bot_Service', 'main', {
+            authentication: cdk.SecretValue.secretsManager('github-token')
+        }),
         commands: [
             'cd project_cdk',
             'npm ci',
