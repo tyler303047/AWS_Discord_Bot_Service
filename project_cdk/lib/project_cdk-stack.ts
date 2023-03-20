d50d0b1b7b32b290bff9e4ebb68f941b66cb676f
tyler303047 <tyler303047@yahoo.com>
2023-03-20 00:29:39 -0500

    Fix try 2

--- project_cdk/lib/project_cdk-stack.ts
+++ project_cdk/lib/project_cdk-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import {GitHubSourceAction} from "aws-cdk-lib/aws-codepipeline-actions";
+import {SecretValue} from "aws-cdk-lib";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ProjectCdkStack extends cdk.Stack {
@@ -11,7 +12,9 @@ export class ProjectCdkStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-          input: CodePipelineSource.gitHub('tyler303047/AWS_Discord_Bot_Service', 'main'),
+          input: CodePipelineSource.gitHub('tyler303047/AWS_Discord_Bot_Service', 'main', {
+              authentication: SecretValue.plainText("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")
+          }),
           installCommands: [
               'npm install -g aws-cdk'
           ],
