9e543116e22466653e2cb273b8d43031258fcd3a
tyler303047 <tyler303047@yahoo.com>
2023-03-22 00:06:07 -0500

    Added maven build for orchestration lambda

--- project_cdk/lib/project_cdk-lambda-stack.ts
+++ project_cdk/lib/project_cdk-lambda-stack.ts
@@ -1,12 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
+import * as os from 'os';
 import { Construct } from "constructs";
 import { Function, InlineCode, Runtime, Code } from "aws-cdk-lib/aws-lambda";
 import * as path from 'path';
-import {aws_lambda} from "aws-cdk-lib";
+import {aws_lambda, DockerVolume} from "aws-cdk-lib";
 
 export class MyLambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, stageName: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        const buildVolume: DockerVolume = {
+            containerPath: '/root/.m2/',
+            hostPath: os.homedir() + '/.m2/',
+        }
+
         new Function(this, 'OrchestrationLambda', {
             runtime: Runtime.JAVA_11,
             handler: 'main.handler',
@@ -14,7 +21,14 @@ export class MyLambdaStack extends cdk.Stack {
                 bundling: {
                     image: aws_lambda.Runtime.JAVA_11.bundlingImage,
                     command: [
-                        ''
+                        '/bin/sh',
+                        '-c',
+                        'cd com/tyler/awsDiscordBot/orchestration ',
+                        '&& mvn clean install ',
+                        '&& cp /asset-input/com/tyler/awsDiscordBot/orchestration/target/orchestration.jar /asset-output/'
+                    ],
+                    volumes: [
+                        buildVolume,
                     ]
                 }
             }),
