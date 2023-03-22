16e1a29b07171aa7c68c467366d096840353bd6c
tyler303047 <tyler303047@yahoo.com>
2023-03-22 01:05:47 -0500

    undid previous change

--- project_cdk/bin/project_cdk.ts
+++ project_cdk/bin/project_cdk.ts
@@ -2,9 +2,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { ProjectCdkPipeline } from '../lib/project_cdk-pipeline';
 
-const app = new cdk.App({
-    outdir: './cdk.out'
-});
+const app = new cdk.App();
 new ProjectCdkPipeline(app, 'ProjectCdkPipeline', {
   env: {
       account: '801301537131',
