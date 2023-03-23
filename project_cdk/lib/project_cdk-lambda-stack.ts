f02a0a6f7e417f59ddc642ea62f5cccbfdf4696d
tyler303047 <tyler303047@yahoo.com>
2023-03-23 00:28:43 -0500

    Adding all of the project infrastructure I've made so far up to this point

--- project_cdk/lib/project_cdk-lambda-stack.ts
+++ project_cdk/lib/project_cdk-lambda-stack.ts
@@ -3,7 +3,7 @@ import * as os from 'os';
 import { Construct } from "constructs";
 import { Function, InlineCode, Runtime, Code } from "aws-cdk-lib/aws-lambda";
 import * as path from 'path';
-import {aws_lambda, DockerVolume} from "aws-cdk-lib";
+import {aws_apigateway, aws_lambda, DockerVolume} from "aws-cdk-lib";
 
 export class MyLambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, stageName: string, props?: cdk.StackProps) {
@@ -14,9 +14,9 @@ export class MyLambdaStack extends cdk.Stack {
             hostPath: os.homedir() + '/.m2/',
         }
 
-        new Function(this, 'OrchestrationLambda', {
+        const orchestrationHandler = new Function(this, 'OrchestrationLambda', {
             runtime: Runtime.JAVA_11,
-            handler: 'main.handler',
+            handler: 'OrchestrationHandler.OrchestrationLambdaHandler::handleRequest',
             code: Code.fromAsset(path.join(__dirname, '/../../src/main/kotlin'), {
                 bundling: {
                     image: aws_lambda.Runtime.JAVA_11.bundlingImage,
@@ -36,6 +36,17 @@ export class MyLambdaStack extends cdk.Stack {
                     ]
                 }
             }),
-        })
+        });
+
+        const apiEntrance = new aws_apigateway.RestApi(this, "awsDiscordBotEntrance", {
+            restApiName: "AWS_Lambda_API Entrance",
+            description: "Interactions endpoint for integration with discord to service requests for my discord bot."
+        });
+
+        const orchestrationIntegration = new aws_apigateway.LambdaIntegration(orchestrationHandler, {
+            requestTemplates: { "application/json": '{ "statusCode": "200" }'}
+        });
+
+        apiEntrance.root.addMethod("GET", orchestrationIntegration);
     }
 }
